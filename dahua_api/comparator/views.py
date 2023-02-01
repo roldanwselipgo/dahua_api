864835725bebccf1267acc8c845943d5ed0083fc
roldan096 <rodrigo096@gmail.com>
2023-02-01 15:22:53 -0600

    Se muestra si hay alguna diferencia por configuracion total del sitio

--- dahua_api/comparator/views.py
+++ dahua_api/comparator/views.py
@@ -22,91 +22,121 @@ class SitioBListView(ListView):
         return sitios
 
 
+    def buscar_sitio_A(self):
+        pass
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         data = []
-        conf = ""
+        #dataA = []
+        #conf = ""
         #Obtener configuraciones del sitio
 
         if 1:
             #for i in range
-            sitios=SitioB.objects.all().order_by('sitio')[:1000]
-            for sitio in sitios:
-                configsB = {}
-                conf = "<br>"
-                #print(sitio.ip, sitio.status)
-                channels=ChannelB.objects.filter(sitio=sitio)
-                #print(channels)
-                for channel in channels:
-                    #print(channel, channel.number)
-                    conf = conf + f"<h5>Channel {channel.number}: <h5>"
-                    #print (channel.streams[0], type(channel.streams))
-                    #stream = StreamB.objects.filter(channel.streams)
-                    streams = channel.streams.all().order_by('name')
-                    
-                    for stream in streams:
-                        st=StreamB.objects.filter(id=stream.id).first()
-                        cf=ConfigB.objects.filter(id=st.id_config.id).first()
-                        #print("stream>",st,cf.Compression)
-                        conf = conf + f"<h6>{st.name}:</h6> "
-                        conf = conf + f"Compression: {cf.Compression} <br>"
-                        conf = conf + f"Resolution: {cf.resolution} <br>"
-                        conf = conf + f"SmartCodec: {cf.SmartCodec} <br>"
-                        conf = conf + f"FPS: {cf.FPS} <br>"
-                        conf = conf + f"BitRateControl: {cf.BitRateControl} <br>"
-                        conf = conf + f"Quality: {cf.Quality} <br>"
-                        conf = conf + f"BitRate: {cf.BitRate} <br>"
-                        conf = conf + f"VideoEnable: {cf.VideoEnable} <br>"
-                        conf = conf + f"Language: {cf.Language} <br>"
-                        conf = conf + f"CurrentTime: {cf.CurrentTime} <br>"
-            #print(conf)
-                configsB["ip"] = sitio.ip
-                configsB["status"] = sitio.status
-                configsB["last_update"] = sitio.last_update
-                #print(conf)
-                configsB["conf"] = conf
-                data.append(configsB)
+            sitiosB=SitioB.objects.all().order_by('sitio')[:1000]
+            # sitiosA=Sitio.objects.all().order_by('sitio')[:1000]
 
-            #Configuracion actuales basadas en las configsB (Base de datos de configuraciones)
-            """sitiosB=SitioB.objects.all().order_by('sitio')[:1000]
-            sitios=Sitio.objects.all().order_by('sitio')[:1000]
-            for sitio in sitios:
-                configsB = {}
-                conf = "<br>"
+            # #Pruebas de comparacion
+            # #sitioPrueba=SitioB.objects.all().order_by('sitio')[:1000].first()
+            # #print("SitioPrueba",sitioPrueba)
+            # #sitioPruebaA=Sitio.objects.filter(sitio=sitioPrueba.sitio).first()
+            # #print("Sitios Prueba", sitioPrueba, sitioPruebaA)
+            # sitiosA = []
+            # for sitioB in sitiosB:
+            #     #sitioA=Sitio.objects.filter(sitio=sitioB.sitio).first()
+            #     sitioA=Sitio.objects.filter(sitio=sitioB.sitio).first()
+            #     sitiosA.append(sitioA) if sitioA else print("No haber sitio")
+            
+            # print("SitiosA: ",sitiosA)
+            # print("Cantidad SitiosA: ",len(sitiosA))
+                    
+
+            # for (sitio,sitioA) in zip(sitiosB,sitiosA):
+            #     #print(f"sitioB: {sitio} , sitioA:{sitioA}")
+            #     pass
+
+            for sitioB in sitiosB:
+                sitioA=Sitio.objects.filter(sitio=sitioB.sitio).first()
+
+                configs = {}
+                confB = "<br>"
+                confA = "<br>"
                 #print(sitio.ip, sitio.status)
-                channels=ChannelB.objects.filter(sitio=sitio)
+                channelsB=ChannelB.objects.filter(sitio=sitioB)
+                channelsA=Channel.objects.filter(sitio=sitioA)
                 #print(channels)
-                for channel in channels:
+                for channelB in channelsB:
                     #print(channel, channel.number)
-                    conf = conf + f"<h5>Channel {channel.number}: <h5>"
+                    confB = confB + f"<h5>Channel {channelB.number}: <h5>"
                     #print (channel.streams[0], type(channel.streams))
                     #stream = StreamB.objects.filter(channel.streams)
-                    streams = channel.streams.all().order_by('name')
+                    streamsB = channelB.streams.all().order_by('name')
                     
-                    for stream in streams:
-                        st=StreamB.objects.filter(id=stream.id).first()
-                        cf=ConfigB.objects.filter(id=st.id_config.id).first()
+                    for streamB in streamsB:
+                        stb=StreamB.objects.filter(id=streamB.id).first()
+                        cfb=ConfigB.objects.filter(id=stb.id_config.id).first()
                         #print("stream>",st,cf.Compression)
-                        conf = conf + f"<h6>{st.name}:</h6> "
-                        conf = conf + f"Compression: {cf.Compression} <br>"
-                        conf = conf + f"Resolution: {cf.resolution} <br>"
-                        conf = conf + f"SmartCodec: {cf.SmartCodec} <br>"
-                        conf = conf + f"FPS: {cf.FPS} <br>"
-                        conf = conf + f"BitRateControl: {cf.BitRateControl} <br>"
-                        conf = conf + f"Quality: {cf.Quality} <br>"
-                        conf = conf + f"BitRate: {cf.BitRate} <br>"
-                        conf = conf + f"VideoEnable: {cf.VideoEnable} <br>"
-                        conf = conf + f"Language: {cf.Language} <br>"
-                        conf = conf + f"CurrentTime: {cf.CurrentTime} <br>"
-            #print(conf)
-                configsB["ip"] = sitio.ip
-                configsB["status"] = sitio.status
-                configsB["last_update"] = sitio.last_update
+                        confB = confB + f"<h6>{stb.name}:</h6> "
+                        confB = confB + f"Compression: {cfb.Compression} <br>"
+                        confB = confB + f"Resolution: {cfb.resolution} <br>"
+                        confB = confB + f"SmartCodec: {cfb.SmartCodec} <br>"
+                        confB = confB + f"FPS: {cfb.FPS} <br>"
+                        confB = confB + f"BitRateControl: {cfb.BitRateControl} <br>"
+                        confB = confB + f"Quality: {cfb.Quality} <br>"
+                        confB = confB + f"BitRate: {cfb.BitRate} <br>"
+                        confB = confB + f"VideoEnable: {cfb.VideoEnable} <br>"
+                        confB = confB + f"Language: {cfb.Language} <br>"
+                        #confB = confB + f"CurrentTime: {cfb.CurrentTime} <br>"
+
+                for channelA in channelsA:
+                    #print(channel, channel.number)
+                    confA = confA + f"<h5>Channel {channelA.number}: <h5>"
+                    #print (channel.streams[0], type(channel.streams))
+                    #stream = StreamB.objects.filter(channel.streams)
+                    streamsA = channelA.streams.all().order_by('name')
+                    for streamA in streamsA:
+                        sta=Stream.objects.filter(id=streamA.id).first()
+                        cfa=Config.objects.filter(id=sta.id_config.id).first()
+                        #print("stream>",st,cf.Compression)
+                        confA = confA + f"<h6>{sta.name}:</h6> "
+                        confA = confA + f"Compression: {cfa.Compression} <br>"
+                        confA = confA + f"Resolution: {cfa.resolution} <br>"
+                        confA = confA + f"SmartCodec: {cfa.SmartCodec} <br>"
+                        confA = confA + f"FPS: {cfa.FPS} <br>"
+                        confA = confA + f"BitRateControl: {cfa.BitRateControl} <br>"
+                        confA = confA + f"Quality: {cfa.Quality} <br>"
+                        confA = confA + f"BitRate: {cfa.BitRate} <br>"
+                        confA = confA + f"VideoEnable: {cfa.VideoEnable} <br>"
+                        confA = confA + f"Language: {cfa.Language} <br>"
+                        #confA = confA + f"CurrentTime: {cfa.CurrentTime} <br>"
+                
+                #Config B
+                configs["idB"] = sitioB.sitio
+                configs["ipB"] = sitioB.ip
+                configs["statusB"] = sitioB.status
+                configs["last_updateB"] = sitioB.last_update
                 #print(conf)
-                configsB["conf"] = conf
-                data.append(configsB)
-            """
-        context["configsB"] = data
+                configs["confB"] = confB
+
+                #Config A
+                configs["idA"] = sitioA.sitio
+                configs["ipA"] = sitioA.ip
+                configs["statusA"] = sitioA.status
+                configs["last_updateA"] = sitioA.last_update
+                configs["confA"] = confA
+
+                if confB == confA:
+                    configs["diff"] = 0
+                else: 
+                    configs["diff"] = 1
+
+
+                #dataB.append(configsB)
+                data.append(configs)
+
+            
+        context["configs"] = data
         print("Finished")
         return context
 
